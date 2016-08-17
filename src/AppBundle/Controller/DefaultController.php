0ea54cdc36e51c91f2a5f3e9977db9c92c499261
Draed <draedper@gmail.com>
2016-08-17 10:11:04 +0400

    2nd commit
    adding the views from older java EE project

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,13 +8,48 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('index.html', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('contact.html', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/regles", name="regles")
+     */
+    public function reglesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('regles.html', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+
+    /**
+     * @Route("/terrain", name="terrain")
+     */
+    public function terrainxAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('terrain.html', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
