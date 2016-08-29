9d6a6686225aadff95b47f7eeaf517026fff8cdb
Draed <draedper@gmail.com>
2016-08-29 09:41:31 +0400

    moving FOS layout and views
    extracting the main FOS file (layout.html.twig)
    now modifications are almost done in the bundle or the app content
    (before was in FOS bundle )

--- app/Resources/views/terrain.php
+++ app/Resources/views/terrain.php
@@ -4,7 +4,7 @@
 		<div class="row">
 		{% for Terrain in listTerrain %}
 	  		 <a href="showDetailTerrain/{{ Terrain.titre }}"><div id="backterrain" class="terrain" class="col-md-12" class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-lg"><div id="showDetailTerrain">{{ Terrain.titre }}</div></div></a>
-	  {% endfor %}		
+	  	{% endfor %}		
 	  	</div>
 	</div>  
 {% endblock %}
